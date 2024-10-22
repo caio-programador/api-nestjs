7411b8c1a8e65df2a9e6ee5e74c7ef536a929fee
Caio Flôres <caio.florees2005@gmail.com>
2024-10-22 11:55:09 -0300

    Utlizando o jest com testes unitários

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,18 +1,123 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoursesService } from './courses.service';
+import { randomUUID } from 'crypto';
+import { CreateCourseDTO } from './dto/create-course.dto';
+import { UpdateCourseDTO } from './dto/update-course.dto';
 
 describe('CoursesService', () => {
   let service: CoursesService;
+  let id: string
+  let created_at: Date
+  let expectOutputTags: any
+  let expectOutputCourses: any
+  let mockCourseRepository: any
+  let mockTagRepository: any
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CoursesService],
-    }).compile();
+    service = new CoursesService()
+    id = randomUUID()
+    created_at = new Date()
+    expectOutputTags = [
+      {
+        id,
+        name: 'nestjs',
+        created_at
+      },
+    ]
+    expectOutputCourses = {
+      id, 
+      name: 'test',
+      description: 'test description',
+      created_at,
+      tags: expectOutputTags
+    }
 
-    service = module.get<CoursesService>(CoursesService);
+    mockCourseRepository = {
+      create: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      save: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      update: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      findAll: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      find: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      findOne: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      remove: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      preload: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+    }
+
+    mockTagRepository = {
+      create: jest.fn().mockReturnValue(Promise.resolve(expectOutputTags)),
+      findOne: jest.fn(),
+    }
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a coures', async () => {
+    //@ts-expect-error
+    service['courseRepository'] = mockCourseRepository
+    //@ts-expect-error
+    service['tagRepository'] = mockTagRepository
+
+    const createCourseDTO: CreateCourseDTO = {
+      name: 'test',
+      description: 'test description',
+      tags: ['nestjs']
+    }
+
+    const newCourse = await service.create(createCourseDTO)
+
+    expect(mockCourseRepository.save).toHaveBeenCalled()
+    expect(expectOutputCourses).toStrictEqual(newCourse)
+  });
+
+  it('should list all courses', async () => {
+    //@ts-expect-error
+    service['courseRepository'] = mockCourseRepository
+
+    const courses = await service.findAll()
+
+    expect(mockCourseRepository.find).toHaveBeenCalled()
+    expect(expectOutputCourses).toStrictEqual(courses)
+  });
+
+  it('should list one course', async () => {
+    //@ts-expect-error
+    service['courseRepository'] = mockCourseRepository
+
+    const course = await service.findOne(id)
+
+    expect(mockCourseRepository.findOne).toHaveBeenCalled()
+    expect(expectOutputCourses).toStrictEqual(course)
+  });
+
+  it('should update a coures', async () => {
+    //@ts-expect-error
+    service['courseRepository'] = mockCourseRepository
+    //@ts-expect-error
+    service['tagRepository'] = mockTagRepository
+
+    const updateCourseDTO: UpdateCourseDTO = {
+      name: 'test',
+      description: 'test description',
+      tags: ['nestjs']
+    }
+
+    const course = await service.update(id,updateCourseDTO)
+
+    expect(mockCourseRepository.save).toHaveBeenCalled()
+    expect(mockCourseRepository.preload).toHaveBeenCalled()
+    expect(expectOutputCourses).toStrictEqual(course)
+  });
+
+  it('should remove one course', async () => {
+    //@ts-expect-error
+    service['courseRepository'] = mockCourseRepository
+
+    const course = await service.remove(id)
+
+    expect(mockCourseRepository.findOne).toHaveBeenCalled()
+    expect(mockCourseRepository.remove).toHaveBeenCalled()
+    expect(expectOutputCourses).toStrictEqual(course)
   });
 });
