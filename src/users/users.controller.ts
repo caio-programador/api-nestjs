ec19494e437c2681a5895d18fd61242b1b175166
Caio Flôres <caio.florees2005@gmail.com>
2024-10-23 10:19:36 -0300

    Implementando signin, signup e listar todos os user para usuários autenticados

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { SignUpDTO } from './dto/signup.dto';
+import { User } from './models/users.model';
+import { SignInDTO } from './dto/signin.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+  
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  public async signup(@Body() signUpDTO: SignUpDTO): Promise<User> {
+    return this.usersService.signUp(signUpDTO)
+  }
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  public async signin(@Body() signInDTO: SignInDTO):
+    Promise<{ name: string, jwtToken: string, email: string }> {
+      return this.usersService.signin(signInDTO)
+  }
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(HttpStatus.OK)
+  public async findAll(): Promise<User[]> {
+    return this.usersService.findAll()
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './models/users.model';
+import { AuthService } from 'src/auth/auth.service';
+import { SignUpDTO } from './dto/signup.dto';
+import { SignInDTO } from './dto/signin.dto';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel('User')
+    private readonly usersModel: Model<User>,
+    private readonly authService: AuthService,
+  ) { }
+
+  public async signUp(signUpDTO: SignUpDTO): Promise<User> {
+    const user = new this.usersModel(signUpDTO)
+    return user.save()
+  }
+
+  public async signin(signInDTO: SignInDTO):
+    Promise<{ name: string; jwtToken: string; email: string }> {
+    const user = await this.findByEmail(signInDTO.email)
+    const match = await this.checkPassword(signInDTO.password, user)
+
+    if (!match) {
+      throw new NotFoundException('Invalid password')
+    }
+
+    const jwtToken = await this.authService.createAccessToken(String(user._id))
+    return {name: user.name, jwtToken: jwtToken, email: user.email}
+
+  }
+
+  public async findAll(): Promise<User[]> {
+    return this.usersModel.find()
+  }
+
+
+  private async findByEmail(email: string): Promise<User> {
+    const user = await this.usersModel.findOne({ email })
+    if (!user) {
+      throw new NotFoundException('Email not found')
+    }
+    return user
+  }
+
+  private async checkPassword(password: string, user: User): Promise<boolean> {
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+      throw new NotFoundException('Wrong password')
+    }
+    return match
+  }
+
+  
+}
