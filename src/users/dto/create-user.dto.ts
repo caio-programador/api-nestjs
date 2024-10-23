bf82b36cabb37fbd7d72bb3f588c5e27ddfe94f0
Caio Flôres <caio.florees2005@gmail.com>
2024-10-23 17:26:18 -0300

    Adicionando o swagger na api

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateUserDto {
+  @ApiProperty({description: 'email do usuario'})
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({description: 'nome do usuario'})
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({description: 'define se o usuario é administrador', default: false})
   @IsBoolean()
   @IsNotEmpty()
   admin: boolean;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,32 +2,44 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiForbiddenResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Rotas de User')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
+  @ApiResponse({
+    status: 409, description: 'Email já cadastrado'
+  })
+  @ApiForbiddenResponse({description: 'Acesso negado'})
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiForbiddenResponse({description: 'Acesso negado'})
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiResponse({status: 404, description: 'Usuário não encontrado'})
+  @ApiForbiddenResponse({description: 'Acesso negado'})
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiResponse({status:400, description:'Bad Request'})
+  @ApiForbiddenResponse({description: 'Acesso negado'})
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiResponse({status:400, description:'Bad Request'})
+  @ApiForbiddenResponse({description: 'Acesso negado'})
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
